37cadaa53b3b8392ddd97ac93405dc926afc0f4a
Lucas N Ferreira <lucasnf99@gmail.com>
2024-08-12 23:54:07 -0300

    fix: adjust icons size

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,49 @@
-import { linksList } from "@/data/linksData";
+import { HomeIcon, InformationCircleIcon, PaperAirplaneIcon, VariableIcon, WalletIcon } from "@heroicons/react/24/outline";
+import FaucetIcon from '@/../public/assets/icons/faucet.svg';
+import ThoughtIcon from '@/../public/assets/icons/thought.svg';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function SideBar() {
   const pathname = usePathname()
 
+  const linksList = [
+    {
+      icon: <HomeIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+      text: 'Home',
+      link: '/'
+    },
+    {
+      icon: <PaperAirplaneIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+      text: 'Send Sol',
+      link: '/send-sol'
+    },
+    {
+      icon: <ThoughtIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900 " />,
+      text: 'Save thoughts',
+      link: '/save-thoughts'
+    },
+    {
+      icon: <FaucetIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900  scale-x-[-1]" />,
+      text: 'Faucet',
+      link: '/faucet'
+    },
+    {
+      icon: <VariableIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+      text: 'Counter',
+      link: '/counter'
+    },
+    {
+      icon: <WalletIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+      text: 'Wallet Status',
+      link: '/wallet-status'
+    },
+    {
+      icon: <InformationCircleIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+      text: 'About',
+      link: '/about'
+    },
+  ];
 
   return (
     <aside className="border-r border-stone-900 dark:border-stone-100 px-6">
--- src/data/linksData.tsx
+++ src/data/linksData.tsx
@@ -3,37 +3,37 @@ import FaucetIcon from '@/../public/assets/icons/faucet.svg';
 import ThoughtIcon from '@/../public/assets/icons/thought.svg';
 export const linksList = [
   {
-    icon: <HomeIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+    icon: <HomeIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
     text: 'Home',
     link: '/'
   },
   {
-    icon: <PaperAirplaneIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+    icon: <PaperAirplaneIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
     text: 'Send Sol',
     link: '/send-sol'
   },
   {
-    icon: <ThoughtIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900 " />,
+    icon: <ThoughtIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900 " />,
     text: 'Save thoughts',
     link: '/save-thoughts'
   },
   {
-    icon: <FaucetIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900  scale-x-[-1]" />,
+    icon: <FaucetIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900  scale-x-[-1]" />,
     text: 'Faucet',
     link: '/faucet'
   },
   {
-    icon: <VariableIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+    icon: <VariableIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
     text: 'Counter',
     link: '/counter'
   },
   {
-    icon: <WalletIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+    icon: <WalletIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
     text: 'Wallet Status',
     link: '/wallet-status'
   },
   {
-    icon: <InformationCircleIcon className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
+    icon: <InformationCircleIcon width={24} className="w-6 h-6 text-stone-900 dark:text-stone-100 group-hover:text-stone-200 group-hover:dark:text-stone-900" />,
     text: 'About',
     link: '/about'
   },
